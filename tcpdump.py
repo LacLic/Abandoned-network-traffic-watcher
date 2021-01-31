5ce89a6157e9f7a47bdc0649056e879b96565455
root <root@LacLic-S-LaPToP.localdomain>
2021-01-31 15:32:37 +0000

    make total traffic output summed up into dict

--- tcpdump.py
+++ tcpdump.py
@@ -1,13 +1,4 @@
 """
-08:18:39.122004 IP 172.19.128.1.54238 > 172.19.137.44.38797: tcp 0
-08:18:39.139868 IP 172.19.128.1.54238 > 172.19.137.44.38797: tcp 329
-08:18:39.140656 IP 172.19.128.1.54238 > 172.19.137.44.38797: tcp 332
-08:18:39.142171 IP 172.19.128.1.54238 > 172.19.137.44.38797: tcp 282
-08:18:39.142963 IP 172.19.128.1.54238 > 172.19.137.44.38797: tcp 285
-08:18:39.184153 IP 172.19.128.1.54238 > 172.19.137.44.38797: tcp 0
-08:18:39.230486 IP 172.19.128.1.54238 > 172.19.137.44.38797: tcp 0
-08:18:39.453056 IP 172.19.128.1.54239 > 172.19.137.44.38797: tcp 111
-08:18:39.472009 IP 172.19.128.1.54239 > 172.19.137.44.38797: tcp 45
 08:18:39.506290 IP 172.19.128.1.54239 > 172.19.137.44.38797: tcp 62
 08:18:39.555459 IP 172.19.128.1.54239 > 172.19.137.44.38797: tcp 0
 08:18:39.560364 IP 172.19.128.1.53 > 172.19.137.44.40162: UDP, length 278
@@ -20,20 +11,30 @@
 08:18:40.129115 IP 13.107.5.93.443 > 172.19.137.44.39216: tcp 1436
 08:18:40.180882 IP 13.107.5.93.443 > 172.19.137.44.39216: tcp 326
 """
+"""
+[('172.19.128.1.54238', 'tcp', '0')]
+[('172.19.128.1.54238', 'tcp', '0')]
+[('172.19.128.1.53', 'UDP', '260')]
+[('172.19.128.1.53', 'UDP', '207')]
+[('172.19.128.1.54238', 'tcp', '0')]
+[('172.19.128.1.54238', 'tcp', '0')]
+"""
 
 def parse_the_echo(text):
     import re
     pattern = re.compile(  # Regex
-      r'IP (.*?) > .*?: (...).*?([0-9][0-9]*)', re.S) # items[0]: source ip 发送方ip，[1]: protocal(previous 3 letters) 协议名（显示前三位），[2]: length 长度
+      r'IP (.*)\.[0-9]*? > .*?: (...).*?([0-9][0-9]*)', re.S) # items[0]: source ip 发送方ip，[1]: protocal(previous 3 letters) 协议名（显示前三个字母），[2]: length 长度
     items = re.findall(pattern, text)
     try:
-        print(items)
-        return items[0],items[1],items[2] # src ip, protocol, length(str)
+        # print(items)
+        return items[0][0],items[0][1],int(items[0][2]) # src ip, port, protocol, length(str)
     except Exception:
         pass
 
 def tcpdump(local_ip,debug=False):
     import subprocess,time
+    global total
+    total = {}
     cmd1 = [f'tcpdump -q -n dst {local_ip}']
     p1 = subprocess.Popen(cmd1,stdout=subprocess.PIPE,encoding='utf-8',shell=True,stderr=subprocess.STDOUT,bufsize=0) # dst: destination 接收方, src: source 发送方
     cmd2 = ['grep', '--line-buffered', '-a', '-o', '-E' , '.*IP.*']
@@ -43,7 +44,13 @@ def tcpdump(local_ip,debug=False):
         echo = p2.stdout.readline().strip().decode('utf-8')
         if debug:
             time.sleep(0.05) # debug, need to be removed
-        parse_the_echo(echo)  # local: 172.19.143.195
+        ip, protocol, lenth = parse_the_echo(echo)  # local: 172.19.143.195
+        try:
+            total[ip] += lenth
+        except KeyError:
+            total[ip] = lenth
+        print(total)
+        
     p.stdout.close()
 
 def get_host_ip():
