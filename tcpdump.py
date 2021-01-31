3dfe6618dab78823763a62e4a4c6dd8851313ace
root <root@LacLic-S-LaPToP.localdomain>
2021-01-31 04:24:29 +0000

    get local host ip and only listen to specific ip

--- tcpdump.py
+++ tcpdump.py
@@ -1,9 +1,31 @@
-def tcpdump():
-    import subprocess,sys,time
-    p = subprocess.Popen('tcpdump -q -n',stdout=subprocess.PIPE,encoding='utf-8',shell=True)
-    # time.sleep(1)
-    sys.stdout.flush()
-    echo = p.stdout.read()
-    print(echo[:-1].split('\n'))  # local: 172.19.143.195
+def tcpdump(local_ip):
+    import subprocess,sys,time,os
+    env = os.environ.copy()
+    env['PYTHONUNBUFFERED'] = '1'
+    p = subprocess.Popen(f'tcpdump -q -n -u dst {local_ip}',stdout=subprocess.PIPE,encoding='utf-8',shell=True,env=env,stderr=subprocess.STDOUT,bufsize=0) # dst: destination 接收方, src: source 发送方
+    while True:
+        echo = p.stdout.readline()
+        time.sleep(0.1)
+        print(echo.strip())  # local: 172.19.143.195
+        # for line in iter(p.stdout.readline, b''):
+        #     print(line)
+    p.stdout.close()
 
-tcpdump()
+# def get_local_ip(): # useless
+#     import socket
+#     return socket.gethostbyname(socket.gethostname())
+
+def get_host_ip():
+    import socket
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+
+    return ip
+
+local_ip = get_host_ip()
+print(local_ip)
+tcpdump(local_ip)
