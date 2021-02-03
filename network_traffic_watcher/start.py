72c326e5fb80cb1281ef7aa247cf04d10e83b8c4
root <root@LacLic-S-LaPToP.localdomain>
2021-02-03 04:29:09 +0000

    build up web and make sync

--- network_traffic_watcher/start.py
+++ network_traffic_watcher/start.py
@@ -1,1 +1,18 @@
-#!/usr/bin/env python3
+"""Failed"""
+# #!/usr/bin/env python3
+# import subprocess
+# import sys
+# cmd1 = [f'python3 {sys.argv[0]}tcpdump.py']
+# p1 = subprocess.Popen(cmd1,stdout=subprocess.STDOUT,stderr=subprocess.STDOUT,encoding='utf-8')
+
+# cmd2 = ['python3',f'{sys.argv[0]}manage.py','runserver','0.0.0.0:8000']
+# p2 = subprocess.Popen(cmd2, stdout=subprocess.PIPE, stdin=p1.stdout)
+
+# print("Programme server started at http://0.0.0.0:8000/")
+# while True:
+#     console = print("Type 'quit' to quit the programme safely:\n  ")
+#     if console == 'quit':
+#         p1.terminate()
+#         p2.terminate()
+#         print("Quit successfully!")
+#         break
