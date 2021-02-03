75106c7ac558198889beed9ccc28653dd1175d21
root <root@LacLic-S-LaPToP.localdomain>
2021-02-03 04:04:14 +0000

    build up web and make sync

---
+++ network_traffic_watcher/tcpdump.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+"""
+08:18:39.506290 IP 172.19.128.1.54239 > 172.19.137.44.38797: tcp 62
+08:18:39.555459 IP 172.19.128.1.54239 > 172.19.137.44.38797: tcp 0
+08:18:39.560364 IP 172.19.128.1.53 > 172.19.137.44.40162: UDP, length 278
+08:18:39.581865 IP 172.19.128.1.53 > 172.19.137.44.40162: UDP, length 179
+08:18:39.631127 IP 13.107.5.93.443 > 172.19.137.44.39216: tcp 0
+08:18:39.637724 IP 172.19.128.1.54239 > 172.19.137.44.38797: tcp 55
+08:18:39.679623 IP 172.19.128.1.54239 > 172.19.137.44.38797: tcp 0
+08:18:39.681551 IP 13.107.5.93.443 > 172.19.137.44.39216: tcp 1436
+08:18:39.829211 IP 13.107.5.93.443 > 172.19.137.44.39216: tcp 1436
+08:18:40.129115 IP 13.107.5.93.443 > 172.19.137.44.39216: tcp 1436
+08:18:40.180882 IP 13.107.5.93.443 > 172.19.137.44.39216: tcp 326
+"""
+"""
+[('172.19.128.1.54238', 'tcp', '0')]
+[('172.19.128.1.54238', 'tcp', '0')]
+[('172.19.128.1.53', 'UDP', '260')]
+[('172.19.128.1.53', 'UDP', '207')]
+[('172.19.128.1.54238', 'tcp', '0')]
+[('172.19.128.1.54238', 'tcp', '0')]
+"""
+"""
+{'172.19.128.1': 20548, '111.221.29.254': 12393, '13.229.188.59': 4552}
+"""
+
+# from func_timeout import func_set_timeout
+# import func_timeout.exceptions
+import subprocess,time
+
+# @func_set_timeout(10)
+def read_out(p2):
+    import json
+    total = {}
+    while True:
+        echo = p2.stdout.readline().strip().decode('utf-8')
+        time.sleep(0.1)
+        print(echo)
+        ip, protocol, lenth = parse_the_echo(echo)  # local: 172.19.143.195
+        try:
+            total[ip] += lenth
+        except KeyError:
+            total[ip] = lenth
+        with open('temp','w') as fio:
+            json.dump(total,fio)
+        print(total)
+
+def parse_the_echo(text):
+    import re
+    pattern = re.compile(  # Regex
+      r'IP (.*)\.[0-9]*? > .*?: (...).*?([0-9][0-9]*)', re.S) # items[0]: source ip 发送方ip，[1]: protocal(previous 3 letters) 协议名（显示前三个字母），[2]: length 长度
+    items = re.findall(pattern, text)
+    try:
+        return items[0][0],items[0][1],int(items[0][2]) # src ip, port, protocol, length(str)
+    except Exception:
+        pass
+
+def tcpdump(local_ip,debug=False):
+
+    cmd1 = [f'tcpdump -q -n dst {local_ip}']
+    # cmd1 = [f'tcpdump -q -n dst {local_ip}']
+    p1 = subprocess.Popen(cmd1,stdout=subprocess.PIPE,encoding='utf-8',shell=True,stderr=subprocess.STDOUT,bufsize=0) # dst: destination 接收方, src: source 发送方
+
+    cmd2 = ['grep', '--line-buffered', '-a', '-o', '-E' , '.*IP.*']
+    p2 = subprocess.Popen(cmd2, stdout=subprocess.PIPE, stdin=p1.stdout)
+    
+    return p2
+
+def get_host_ip():
+    import socket
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+    return ip
+
+def start_process():
+    local_ip = get_host_ip()
+    return tcpdump(local_ip,debug=False)
+
+p = start_process()
+read_out(p)
